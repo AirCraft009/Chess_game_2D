a499db552e2ed1f93701036710930ad2f0ae5ddc
Mxsxll <coconraupe@gmail.com>
2025-02-07 00:12:45 +0100

    another one

--- Chess_logic.py
+++ Chess_logic.py
@@ -2,6 +2,27 @@ import pygame
 from fen_read import read_Fen
 from possible_moves import poss_moves
 
+class Piece(object):
+    def __init__(self, x, y, color, type, step, value, image, space):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = type
+        self.step = step
+        self.value = value
+        self.image = image
+        self.space = space
+        self.selected = False
+        self.moved = False
+        
+        
+    def render(self):
+        	
+        screen.blit(self.image, (self.x, self.y))
+        if self.selected:
+            pygame.draw.rect(screen, ("red"), (self.x, self.y, 100, 100), 5)
+            pygame.display.update()
+
 
 
 
@@ -32,8 +53,8 @@ while True:
         except:
             print("invalid fen")
     elif fen == "y":
-        fen = "rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"
-        board = read_Fen(fen, board) if c else read_Fen(fen, board)  
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/7R"
+        board = read_Fen(fen, board) if not c else read_Fen(fen[::-1], board)  
         # print(board)
         break
     else:
@@ -46,6 +67,7 @@ screen = pygame.display.set_mode((800 , 800))
 pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
 pygame.init()
+pos_board_pieces = {}
 pos_board = {}
 board_pieces = {}
 
@@ -84,22 +106,23 @@ def piece_board(board, allocated_board):
     piece_map = {
         2:  ("white", 1, 1, white_Pawn),
         3:  ("black", 1, 1, black_Pawn),
-        4:  ("white", 2, 2, white_Knight),
-        5:  ("black", 2, 2, black_Knight),
+        4:  ("white", 2, 3, white_Knight),
+        5:  ("black", 2, 3, black_Knight),
         8:  ("white", 3, 3, white_Bishop),
         9:  ("black", 3, 3, black_Bishop),
-        16: ("white", 4, 4, white_Rook),
-        17: ("black", 4, 4, black_Rook),
-        32: ("white", 5, 5, white_Queen),
-        33: ("black", 5, 5, black_Queen),
-        64: ("white", 6, 6, white_King),
-        65: ("black", 6, 6, black_King),
+        16: ("white", 4, 5, white_Rook),
+        17: ("black", 4, 5, black_Rook),
+        32: ("white", 5, 9, white_Queen),
+        33: ("black", 5, 9, black_Queen),
+        64: ("white", 6, 30, white_King),
+        65: ("black", 6, 30, black_King),
     }
     
-    for x, piece in board.items():
+    for x in board:
+        piece = board[x]
         if piece in piece_map:
             color, value1, value2, image = piece_map[piece]
-            allocated_board[x] = Piece(pos_board[x][0], pos_board[x][1], color, piece, value1, value2, image, x)
+            allocated_board[x] = Piece(pos_board_pieces[x][0], pos_board_pieces[x][1], color, piece, value1, value2, image, x)
     return allocated_board
     
     
@@ -119,36 +142,22 @@ def draw_board():
         for x in range(8):
             for y in range(8):
                 #the logic works because it is a 8x8 board and the screen is 800x800
-                pygame.draw.rect(screen, ("grey" if y % 2 - x % 2 == 0 else "teal"), (y*100 , x * 100,  100, 100))
+                pygame.draw.rect(screen, ("teal" if y % 2 - x % 2 == 0 else "grey"), (y*100 , x * 100,  100, 100))
                 if board[x + y * 8] != 0:
+                    pos_board_pieces[x + y * 8] = ((700 - x * 100 , 700 - y * 100))
                     pos_board[x + y * 8] = ((x * 100 , y * 100))
     else:
         for x in range(8):
             for y in range(8):
-                pygame.draw.rect(screen, ("teal" if y % 2 - x % 2 == 0 else "grey"), (y*100 , x * 100,  100, 100))
+                pygame.draw.rect(screen, ("grey" if y % 2 - x % 2 == 0 else "teal"), (y*100 , x * 100,  100, 100))
                 if board[x + y * 8] != 0:
+                    pos_board_pieces[x + y * 8] = ((700 - x * 100 ,700 - y * 100))
                     pos_board[x + y * 8] = ((x * 100 , y * 100))
             
            
-class Piece(object):
-    def __init__(self, x, y, color, type, step, value, image, space):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.type = type
-        self.step = step
-        self.value = value
-        self.image = image
-        self.space = space
-        self.selected = False
-        self.moved = False
+
     
-    def render(self, depth = 1):
-        	
-        screen.blit(self.image, (self.x, self.y))
-        if self.selected:
-            pygame.draw.rect(screen, ("red"), (self.x, self.y, 100, 100), 5)
-            pygame.display.update()
+
 
 
 def render_pieces(board):        
@@ -179,6 +188,7 @@ while True:
             r_posy = posy//100
             
             
+            
             if clicked:
                 print("clicked")
                 if board[r_posx + r_posy * 8] == 0 or board[r_posx + r_posy * 8]%2  != piecevalue%2: 
@@ -200,10 +210,11 @@ while True:
                     clicked = True
                     prev_x = r_posx
                     prev_y = r_posy
-                    piecevalue = board[r_posx + r_posy * 8]
-                    board_pieces[r_posx + r_posy * 8].selected = False if board_pieces[r_posx + r_posy * 8].selected else True
+                    piecevalue = board[r_posx +  r_posy * 8]
+                    board_pieces[63 -r_posx + 63 -r_posy * 8].selected = False if board_pieces[63 - r_posx +  63 - r_posy * 8].selected else True
                     draw_board()
                     render_pieces(board_pieces)
+                    print(piecevalue)
     pygame.display.flip()
 
     # pygame.init()
